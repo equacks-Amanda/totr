a69bcdbb53d6b0ea55e269b464dfa80eee83f3cb
equacks_Amanda <a.barbadora@gmail.com>
2018-05-29 11:04:21 -0400

    Ice Wall dissolves. Text changes on warmup menus.

--- Assets/Scripts/IceWallController.cs
+++ Assets/Scripts/IceWallController.cs
@@ -5,11 +5,30 @@
  */
 
 using UnityEngine;
+using System.Collections;
 
 public class IceWallController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject OnDestroyParticle;
+
+    [SerializeField]
+    private Collider boxCollider;
+
+    [SerializeField]
+    ShaderEffect dissolveWall;
+
 #region Unity Overrides
     void Start () {
-		Destroy(gameObject, Constants.SpellStats.C_IceWallLiveTime);
+        //Destroy(gameObject, Constants.SpellStats.C_IceWallLiveTime);
+        StartCoroutine(WaitToDestroy());
 	}
+
+    IEnumerator WaitToDestroy()
+    {
+         yield return new WaitForSeconds(Constants.SpellStats.C_IceWallLiveTime);
+        boxCollider.enabled = false;
+        dissolveWall.ParamIncrease(3f, true, "_DisintegrateAmount");
+    }
 #endregion
 }
